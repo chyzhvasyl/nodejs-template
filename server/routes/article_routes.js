4cfe2faa74965f4a425676e8a8a50f908b0b69bb
Andrey <andreygazhala@gmail.com>
2018-07-30 14:22:07 +0300

    Changed syntax error in emailOrTelephone field(comment model)

--- server/routes/article_routes.js
+++ server/routes/article_routes.js
@@ -16,13 +16,13 @@ var storage = multer.diskStorage({
 
 var upload = multer({
   storage: storage,
-  limits:{fileSize: 100000000000},
-  fileFilter: function(req, file, cb) {
+  limits: { fileSize: 100000000000 },
+  fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
-  } 
+  }
 }).single('img');
 
-function checkFileType(file, cb){
+function checkFileType(file, cb) {
   const filetypes = /jpeg|jpg|png|gif/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
@@ -47,16 +47,16 @@ function findAllArticles(req, res) {
   Article.find()
     .populate('comments')
     .populate('category')
-    .exec(function(err, articles) {
-    if(err) {
-      res.status(404);
-      res.json(err);
-      intel.error(err);
-    } else {
-      res.json(articles);
-      intel.info("Get all articles ", articles);
-    }
-  });
+    .exec(function (err, articles) {
+      if (err) {
+        res.status(404);
+        res.json(err);
+        intel.error(err);
+      } else {
+        res.json(articles);
+        intel.info("Get all articles ", articles);
+      }
+    });
 }
 
 // *** get SINGLE article by id *** //
@@ -64,33 +64,33 @@ function findArticleById(req, res) {
   Article.findById(req.params.id)
     .populate('comments')
     .populate('category')
-    .exec(function(err, article) {
-    if(err) {
-      res.status(400);
-      res.json(err);
-      intel.error(err);
-    } else {
-      res.json(article);
-      intel.info('Get single article by id ', article);
-    }
-  });
+    .exec(function (err, article) {
+      if (err) {
+        res.status(400);
+        res.json(err);
+        intel.error(err);
+      } else {
+        res.json(article);
+        intel.info('Get single article by id ', article);
+      }
+    });
 }
 
 // *** get All articles by category *** //
-function findAllArticlesByCategory(req, res) {  
-  Article.find({'category':req.params.category_id})
-  .populate('comments')
-  .populate('category')
-  .exec(function(err, articles){
-    if(err) {
-      res.status(400);
-      res.json(err);
-      intel.error(err);
-    } else {
-      res.json(articles);
-      intel.info("Get all articles by category" + req.params.category, articles);
-    }
-  });
+function findAllArticlesByCategory(req, res) {
+  Article.find({ 'category': req.params.category_id })
+    .populate('comments')
+    .populate('category')
+    .exec(function (err, articles) {
+      if (err) {
+        res.status(400);
+        res.json(err);
+        intel.error(err);
+      } else {
+        res.json(articles);
+        intel.info("Get all articles by category" + req.params.category, articles);
+      }
+    });
 }
 
 // *** add SINGLE article  *** //
@@ -109,26 +109,26 @@ function addArticle(req, res) {
         newArticle.img.name = req.file.filename;
         newArticle.img.data = fs.readFileSync(req.file.path);
         newArticle.img.contentType = req.file.mimetype;
-      } 
+      }
       newArticle.timeOfCreation = req.body.timeOfCreation;
       newArticle.timeOfPublication = req.body.timeOfPublication;
       newArticle.category = req.body.category;
       newArticle.confirmation = req.body.confirmation;
       newArticle.status = req.body.status;
       newArticle.category = req.params.category_id;
-      newArticle.save(function(err, newArticle) {
+      newArticle.save(function (err, newArticle) {
         if (err) {
           res.sendStatus(400);
           res.json(err);
           intel.error(err);
-        } else { 
+        } else {
           res.json(newArticle);
           intel.info('Added new article ', newArticle);
         }
-      }); 
-    } 
+      });
+    }
   });
-};  
+};
 
 // *** update SINGLE article *** //
 function updateArticle(req, res) {
@@ -139,16 +139,16 @@ function updateArticle(req, res) {
       intel.error(err);
       return
     } else {
-      Article.findById(req.params.id, function(err, article) {      
+      Article.findById(req.params.id, function (err, article) {
         if (req.body.title) {
           article.title = req.body.title;
         }
         if (req.body.body) {
-           article.body = req.body.body;
+          article.body = req.body.body;
         }
         if (req.file) {
           article.img.name = req.file.filename,
-          article.img.data = fs.readFileSync(req.file.path);
+            article.img.data = fs.readFileSync(req.file.path);
           article.img.contentType = req.file.mimetype;
         }
         if (req.body.timeOfCreation) {
@@ -163,16 +163,11 @@ function updateArticle(req, res) {
         if (req.body.status) {
           article.status = req.body.status;
         }
-        if (req.params.is_liked == false) {
-          article.likes++;
-        } else {
-          article.likes--;
-        }
         if (req.params.category_id) {
           article.category = req.params.category_id;
         }
-        article.save(function(err) {
-          if(err) {
+        article.save(function (err) {
+          if (err) {
             res.status(400);
             res.json(err);
             intel.error(err);
@@ -187,34 +182,32 @@ function updateArticle(req, res) {
 }
 
 function likeArticle(req, res) {
-  console.log(req.params);
   let likeAction;
-  if(req.params.is_liked == 'false') {
-    likeAction = { $inc: { likes: 1 }};
+  if (req.params.is_liked == 'false') {
+    likeAction = { $inc: { likes: 1 } };
   } else {
-    likeAction = { $inc: { likes: -1 }};
+    likeAction = { $inc: { likes: -1 } };
   }
-  console.log(likeAction);
-  Article.findOneAndUpdate(req.params.id, likeAction, function(err, article) {
+  Article.findOneAndUpdate(req.params.id, likeAction, function (err, article) {
     if (err) {
       res.status(400);
       res.json(err);
       intel.error(err);
-     } else {
+    } else {
       res.json(article);
-     }
+    }
   })
 }
 
 // *** delete SINGLE article *** //
 function deleteArticle(req, res) {
-  Article.findByIdAndDelete(req.params.id, function(err, article) {
-    if(err) {
+  Article.findByIdAndDelete(req.params.id, function (err, article) {
+    if (err) {
       res.json(err);
     } else {
-        res.json(article);
-        intel.info('Deleted article ', article);
-      }
+      res.json(article);
+      intel.info('Deleted article ', article);
+    }
   });
 }
 
